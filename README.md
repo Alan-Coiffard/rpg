0fceff3a5e1ef2d82c48b732d2f8a8721d344515
Alan Coiffard <alan.coiffar@gmail.com>
2021-09-28 16:41:56 +0200

    Aide à 'linstalation et au jeu

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # rpg
+
+## Installation
+
+```sh
+- git clone https://github.com/Alan-Coiffard/rpg
+
+- cd /path
+
+- make -f regles
+```
+
+## Utilisation
+
+```sh
+./jeu
+```
--- rpg.c
+++ rpg.c
@@ -12,32 +12,45 @@ void start(){
 	
 	//init du perso principal
 	perso_s monPerso = initPerso();
-	voirPerso(&monPerso);
+	//voirPerso(&monPerso);
 
 	//init de l'adversaire de facon alea
 	adversaire_s monAdversaire = initAdversaire("Un", "méchant");
 
-	//mise en place du combat
-	//combat(&monPerso, &monAdversaire);
-
-	//création d'objet et test de l'ajout et suppression	
+	//création d'objet	
 	objet_s premierObjet = creerObjet("chaise", 10, 10);
 	objet_s lourdObjet = creerObjet("table", 120, 210);
-	//afficherObjet(&premierObjet);
-	//afficherObjet(&lourdObjet);
-	//ajoutObjet(&premierObjet, &monPerso);
-	//voirPerso(&monPerso);
-	//ajoutObjet(&lourdObjet, &monPerso);
-	//voirPerso(&monPerso);
 
-//definition des arme avec arguments
-//arme(char nom[], int poid, int dureeVie, int degats, int distance);
-
-/*
+	//création arme
 	armes_s monArme = creerArme("épée", 5, 50, 25, 0);
-	prendreArme(&monPerso, &monArme);
-	voirPerso(&monPerso);
-*/
+
+	clear();
+	int reponse;
+	
+	while(1){
+
+		printf("Pour aller plus loins : \n");
+		printf("-1 - stop\n");
+		printf("0 - combat\n");
+		printf("1 - arme\n");
+		printf("2 - objets\n");
+		printf("3 - afficher perso\n");
+		scanf("%d", &reponse);
+		if (reponse == -1)
+		{
+			/* code */
+			break;
+		}
+		switch(reponse)
+	   	{
+		   	case 0 : clear(); combat(&monPerso, &monAdversaire); 	break;
+		   	case 1 : clear(); prendreArme(&monPerso, &monArme); 	break;
+		   	case 2 : clear(); ajoutObjet(&lourdObjet, &monPerso);	break;
+		   	case 3 : clear(); voirPerso(&monPerso);					break;
+		   	default : printf("erreur interne du logiciel numéro %d invalide\n", reponse);
+	   	}
+	}
+
 
 
 }
